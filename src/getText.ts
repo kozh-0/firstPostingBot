b07580d26c84802e47085352686100a89035ca9d
Дмитрий А. Кожевников <kozhevnikov_d@ufanet.ru>
2024-03-21 19:57:53 +0500

    добавил yandexgpt, который пытается сформировать пост и отправить в новый тг канал

--- src/getText.ts
+++ src/getText.ts
@@ -1,3 +1,52 @@
-export const generateText = (): string => {
-  return Buffer.alloc(Math.random() * 100, "pwerhzsjov").toString("hex");
+import axios from "axios";
+
+const FOLDER_ID = process.env.FOLDER_ID;
+const YANDEXGPT_API_KEY = process.env.YANDEXGPT_API_KEY;
+
+const data = {
+  modelUri: `gpt://${FOLDER_ID}/yandexgpt-lite/latest`,
+  completionOptions: {
+    stream: false,
+    temperature: 0.6,
+    maxTokens: "500",
+  },
+  messages: [
+    {
+      // ХРЕНОВО РАБОТАЕТ, результат в 1% случаев, неправильный промпт?
+      role: "system",
+      text: "Ты опытный кинолог и знаешь все факты о собаках",
+      // text: "Напиши один интересный факт о собаках с загаловком, нужно уложиться в 1024 символа",
+    },
+    // {
+    //   role: "assistant",
+    //   text: "Собаки очень милые и интересные создания",
+    // },
+    {
+      role: "assistant",
+      text: "Напиши один интересный факт о собаках с загаловком",
+    },
+  ],
 };
+
+export async function generateDogPost() {
+  try {
+    const response = await axios.post(
+      "https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
+      data,
+      {
+        headers: {
+          Authorization: `Api-Key ${YANDEXGPT_API_KEY}`,
+          "x-folder-id": FOLDER_ID,
+        },
+      }
+    );
+    console.log(response.status, new Date());
+    console.log(JSON.stringify(response.data, null, 2));
+
+    return response.data.result.alternatives[0].message.text ?? "Что-то пошло не так...";
+  } catch (err) {
+    // @ts-ignore
+    console.error("error:", err.message);
+    return "Фатальная ошибка";
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import { facts } from "./facts";
 import axios from "axios";
-// import { generateText } from "./getText";
+import { generateDogPost } from "./getText";
 
 const bot = new Telegraf(process.env.TG_KEY!, { handlerTimeout: 20000 });
 
@@ -16,30 +16,42 @@ bot.on(message("text"), async (ctx) => {
   if (!input) return;
 
   //   const text = generateText();
-  //   ctx.telegram.sendPhoto(process.env.CHANNEL_NAME!, temporaryImageLink, { caption: text });
+  //   ctx.telegram.sendPhoto(process.env.!, temporaryImageLink, { caption: text });
   ctx.reply("Отправлено: " + input);
 });
 
-let count = 17;
+let count = 24;
 const interval = setInterval(async () => {
   if (!facts[count]) {
     clearInterval(interval);
     return bot.telegram.sendMessage(
-      process.env.CHANNEL_NAME!,
+      process.env.CATS_CHANNEL_NAME!,
       "Посты временно закончились... \n\n Скоро будут новые факты!🐱"
     );
   }
 
-  const img: [{ id: string; url: string; width: number; height: number }] = await axios(
+  const catImg: [{ id: string; url: string; width: number; height: number }] = await axios(
     `https://api.thecatapi.com/v1/images/search`
   ).then((res) => res.data);
 
-  bot.telegram.sendPhoto(process.env.CHANNEL_NAME!, img[0].url, {
+  bot.telegram.sendPhoto(process.env.CATS_CHANNEL_NAME!, catImg[0].url, {
     caption: `${count + 1}. ${facts[count].title}\n\n${facts[count].descr}`,
   });
 
   count += 1;
   console.log(count);
+
+  const dogImg: { message: string; status: string } = await axios(
+    `https://dog.ceo/api/breeds/image/random`
+  ).then((res) => res.data);
+
+  console.log("В работе");
+  const post = await generateDogPost();
+  console.log("Получил, отправляю...", post);
+
+  bot.telegram.sendPhoto(process.env.DOGS_CHANNEL_NAME!, dogImg.message, {
+    caption: post,
+  });
 }, 1800000);
 
 // bot.on("voice", (ctx) => ctx.reply("Какой чудный у вас голос 😉"));
