45988a71b7d699aece8f932c7c9f0123745f7480
Дмитрий А. Кожевников <kozhevnikov_d@ufanet.ru>
2024-04-03 11:57:13 +0500

    хэштег звуки + readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Должна быть создана папка imgs
+Должна быть создана папка imgs и файл counter.txt с указание числа последнего поста в тг канале
+
+Нужно заполнить .env
+
+```sh
+USER_TG_ID=
+TG_KEY=
+CATS_CHANNEL_NAME=
+DOGS_CHANNEL_NAME=
+
+YANDEXGPT_FOLDER_ID=
+YANDEXGPT_API_KEY=
+
+GIGACHAT_CLIENT_ID=
+GIGACHAT_CLIENT_SECRET=
+GIGACHAT_AUTH_SECRET=
+```
--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,9 @@ async function kotikPost() {
   let DB_COUNTER = parseInt(fs.readFileSync("counter.txt", { encoding: "utf-8" })) + 1;
 
   bot.telegram
-    .sendPhoto(process.env.CATS_CHANNEL_NAME!, catImg[0].url, { caption: `${DB_COUNTER}. ${fact}` })
+    .sendPhoto(process.env.CATS_CHANNEL_NAME!, catImg[0].url, {
+      caption: `${DB_COUNTER}. ${fact.toLowerCase().includes("звук") ? fact + "\n\n#Звуки" : fact}`,
+    })
     .then(() => {
       fs.writeFileSync("counter.txt", JSON.stringify(DB_COUNTER));
     });
